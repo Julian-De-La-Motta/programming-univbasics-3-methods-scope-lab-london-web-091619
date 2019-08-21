6bbdf750b3338cd9487f59adec567214266fabf3
glorious-communicator-0403 <juliandelamotta@gmail.com>
2019-08-21 15:36:09 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,9 +8,14 @@ def toadstool
   puts $status
 end
 
+def link
+puts "It's Dangerous To Go Alone! Take This"
+end
+
 def any_phrase(phrase)
- # puts "takes in an argument and puts out a catchphrase"
-  phraseout = phrase
+ puts "takes in an argument and puts out a catchphrase"
+   puts "There's no time #{phrase}"
+  phraseout = "Do A Barrel Roll!"
   puts phraseout
 end
 
