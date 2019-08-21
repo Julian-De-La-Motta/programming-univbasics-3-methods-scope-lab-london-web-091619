b255d30a3d6eab668eb7f5d1ed6f7aa0a3dd3205
glorious-communicator-0403 <juliandelamotta@gmail.com>
2019-08-21 14:38:08 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,6 @@
-def mario(name)
+def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  phrase = "It's-a me, #{name}!"
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
